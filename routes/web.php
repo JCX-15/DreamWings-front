964793f6ac01a9fc139c29234466e533557b8709
AlejoZeron <ale25.zeron@gmail.com>
2024-04-21 22:28:05 -0600

    login bueno

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function (){
-    return view('login');
-});
-
-Route::get('login', [loginController::class, 'login'])->name(('login'));
+Route::get('/login', [loginController::class, 'login'])->name(('login'));
