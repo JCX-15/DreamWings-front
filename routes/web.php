a0cede6bc4b89fde511aba56dbc889b2a73137be
AlejoZeron <ale25.zeron@gmail.com>
2024-04-21 22:15:59 -0600

    ruta login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Expr\FuncCall;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function (){
+    return view('login');
 });
